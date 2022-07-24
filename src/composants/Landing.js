c49f02ffbff16f06131d1cca9efe931aa74d54fc
Tsinjo Elie <elie.tsinjo@gmail.com>
2022-07-24 17:25:59 +0100

    Modification du template

---
+++ src/composants/Landing.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import View from "./View";
+class Landing extends Component () {
+    render () {
+        return (
+            <View />
+        )
+    }
+}
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Landing from './composants/Landing';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const wrapper = ReactDOM.createRoot(document.getElementById('wrapper'));
+wrapper.render(
   <React.StrictMode>
-    <App />
+    <Landing />
   </React.StrictMode>
 );
 
